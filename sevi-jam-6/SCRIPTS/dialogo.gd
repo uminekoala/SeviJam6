1ce4b1c93814cf61e22edc9100dc58390eb14d2b
Servelgar <svelazquezg96@gmail.com>
2026-06-27 18:32:17 +0200

    miau

--- sevi-jam-6/SCRIPTS/dialogo.gd
+++ sevi-jam-6/SCRIPTS/dialogo.gd
@@ -11,14 +11,14 @@ func _ready() -> void:
 # Start typing the provided message
 func update_message(message: String) -> void:
 	$AudioStreamPlayer.stop()
+	$AudioStreamPlayer.play()
 	content.text = message
 	content.visible_characters = 0
 	type_timer.start()
 	while content.visible_characters < content.text.length():
 		content.visible_characters += 1
 		voice_player.pitch_scale = randf_range(0.95, 1.08) #para darle más jugo
-		$AudioStreamPlayer.play()
-		await get_tree().create_timer(0.1).timeout
+		await get_tree().create_timer(0.03).timeout
 	$AudioStreamPlayer.stop()
 
 func _enter_tree() -> void:
