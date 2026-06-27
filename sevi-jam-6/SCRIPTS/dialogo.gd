51aa9b53494e2c594d1a474a19314a6e89948cff
Dew <andrea.rs.98@gmail.com>
2026-06-27 13:07:05 +0200

    URG

---
+++ sevi-jam-6/SCRIPTS/dialogo.gd
@@ -0,0 +1,24 @@
+class_name Dialogue
+extends Control
+ 
+@onready var voice_player := get_node("AudioStreamPlayer") as AudioStreamPlayer
+@onready var content := get_node("texto") as RichTextLabel
+@onready var type_timer := get_node("TypeTyper") as Timer
+
+func _ready() -> void:
+	await get_tree().create_timer(1.0).timeout
+	update_message("Lepers coiled underneath the trees...")
+
+# Start typing the provided message
+func update_message(message: String) -> void:
+	content.text = message
+	content.visible_characters = 0
+	type_timer.start()
+	while content.visible_characters < content.text.length():
+		content.visible_characters += 1
+		voice_player.pitch_scale = randf_range(0.95, 1.08) #para darle más jugo
+		$AudioStreamPlayer.play()
+		await get_tree().create_timer(0.1).timeout
+	$AudioStreamPlayer.stop
+
+	
