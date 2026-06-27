ba40d8fd8ce4fae69ae55250748d9f4f0e2082a9
Servelgar <svelazquezg96@gmail.com>
2026-06-27 09:26:39 +0200

    miau

---
+++ sevi-jam-6/SCRIPTS/orb_clicker.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _input(event) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		Global.is_gameplay = true
+	else:
+		Global.is_gameplay = false
